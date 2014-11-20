03f3d8c803addd66ddd7ce15f78c56e8e32cf489
Martin Stiehr <git@maltin.pisces.uberspace.de>
2014-11-20 09:29:38 +0100

    changed procedure of buffering input file

--- deduplicate.c
+++ deduplicate.c
@@ -89,72 +89,88 @@ int main(int argc, char **argv) {
 	void *journalMapCurrentAdd = journalMapAdd + journalFileLen; // Hilfszeiger soll ans Dateiende zeigen 
 	// STATISTIK
 	off_t journalEntries = journalFileLen / sizeof(journalentry);	
-	
+
+
+// BEGINN DER VERARBEITUNG
 startZeit = time(NULL);	
-	// DIE EINGABEDATEI EINLESEN 
-	char *inputFileBuffer = malloc(sizeof(char)*inputFileLen);
-	int i=0;
-	while(fread(inputFileBuffer+i*100*CHUNKSIZE, CHUNKSIZE, 100, inputFile))
-		i++;
-	// FÜR JEDEN CHUNK EINEN HASH BERECHNEN UND MIT DEM JOURNAL VERGLEICHEN 
-	char md5String[32+1];
-	int current_read = 0; // wie viele Bytes aktuell vorhanden sind 
-	size_t bytesRead; // Summe der konsumierten Bytes 
-	long run = 0;  // Durchlauf
-	long newBytes = 0; // Blöcke, die neu ins Journal aufgenommen wurden 
-	char metaFileChanged;
-	long infoForMetaFile;
-	MD5_CTX md5Context;
-	unsigned char md[16];
-	printf("deduplicating \"%s\" [%.3f MB]\n",inputFileName, inputFileLenMB);
-	for(bytesRead=0; bytesRead<inputFileLen;) {
-		metaFileChanged = FALSE;
-		current_read = (CHUNKSIZE<=(inputFileLen-bytesRead)) ? CHUNKSIZE : inputFileLen-bytesRead;
-		if(MD5_Init(&md5Context)==0) {
-			perror("MD5_Init()");
+char * inputFileBuffer;
+	// DIE EINGABEDATEI EINLESEN
+	unsigned int bytesBufferSize = 10*1024*1024; // 10 MB
+	off_t bytesActuallyRead = 0L;
+	for(long bytesBuffered = 0L; bytesBuffered<inputFileLen; bytesBuffered += bytesBufferSize) {
+		inputFileBuffer = malloc(sizeof(char)*bytesBufferSize);
+		if(inputFileBuffer==NULL) {
+			perror("ERROR: could not allocate memory");
 			exit(1);
 		}
-		MD5_Update(&md5Context, inputFileBuffer+bytesRead, current_read);
-		MD5_Final(md, &md5Context);
-		for(i=0;i<16;i++) 
-			sprintf(md5String+2*i, "%02x", (unsigned int) md[i]);	
-		// Testen, ob der errechnete Hash bereits bekannt ist
-		long hashInJournalPos = isHashInMappedJournal(md5String, journalMapAdd, journalEntries);
-		if(hashInJournalPos==-1) { // DER HASH IST UNBEKANNT -> MUSS ANGEFÜGT WERDEN 
-//printf("!");
-			infoForMetaFile = journalEntries; // in diesem Datensatz wird sich der neue Hash befinden
-			journalentry record; // neuen Eintrag bauen 
-			record.block = storageFileLen;
-			strncpy(record.hash, md5String, 32+1);
-			record.len = current_read;
-			fwrite(inputFileBuffer, current_read, 1, storageFile); // Daten anfügen
-			memcpy(journalMapCurrentAdd, &record, sizeof(journalentry)); // Eintrag im Journal
-			journalMapCurrentAdd += sizeof(journalentry);
-			metaFileChanged = TRUE;
-			journalEntries++;
-			if(journalEntries*sizeof(journalentry) >= journalMapLen) {
-			// die Journal-Datei muss vergrößert und erneut gemappt werden 
-				munmap(journalMapAdd, journalMapLen);
-				journalMapAdd = mapFile(fileno(journalFile),journalMapLen, auxSpace, &journalMapLen);
-				journalMapCurrentAdd = journalMapAdd + journalEntries*sizeof(journalentry);
+		bytesActuallyRead = fread(inputFileBuffer,bytesBufferSize,1,inputFile);
+
+	// FÜR JEDEN CHUNK EINEN HASH BERECHNEN UND MIT DEM JOURNAL VERGLEICHEN 
+		char md5String[32+1];
+		int current_read = 0; // wie viele Bytes aktuell vorhanden sind 
+		size_t bytesRead; // Summe der konsumierten Bytes 
+		long run = 0;  // Durchlauf
+		long newBytes = 0; // Blöcke, die neu ins Journal aufgenommen wurden 
+		char metaFileChanged;
+		long infoForMetaFile;
+		MD5_CTX md5Context;
+		unsigned char md[16];
+		printf("deduplicating \"%s\" [%.3f MB]\n",inputFileName, inputFileLenMB);
+		for(bytesRead=0; bytesRead<bytesActuallyRead;) {
+			metaFileChanged = FALSE;
+			current_read = (CHUNKSIZE<=(bytesActuallyRead-bytesRead)) ? CHUNKSIZE : bytesActuallyRead-bytesRead;
+			if(MD5_Init(&md5Context)==0) {
+				perror("MD5_Init()");
+				exit(1);
 			}
-			//printf("%li;%32s;%i\n",record.block, md5String, record.len);
-		} else { // DER HASH IST BEREITS BEKANNT
-//printf(".");
-			infoForMetaFile = hashInJournalPos;
+			MD5_Update(&md5Context, inputFileBuffer+bytesRead, current_read);
+			MD5_Final(md, &md5Context);
+			for(i=0;i<16;i++) 
+				sprintf(md5String+2*i, "%02x", (unsigned int) md[i]);	
+			// Testen, ob der errechnete Hash bereits bekannt ist
+			long hashInJournalPos = isHashInMappedJournal(md5String, journalMapAdd, journalEntries);
+			if(hashInJournalPos==-1) { // DER HASH IST UNBEKANNT -> MUSS ANGEFÜGT WERDEN 
+	//printf("!");
+				infoForMetaFile = journalEntries; // in diesem Datensatz wird sich der neue Hash befinden
+				journalentry record; // neuen Eintrag bauen 
+				record.block = storageFileLen;
+				strncpy(record.hash, md5String, 32+1);
+				record.len = current_read;
+				fwrite(inputFileBuffer+bytesRead, current_read, 1, storageFile); // Daten anfügen
+				memcpy(journalMapCurrentAdd, &record, sizeof(journalentry)); // Eintrag im Journal
+				journalMapCurrentAdd += sizeof(journalentry);
+				metaFileChanged = TRUE;
+				journalEntries++;
+				if(journalEntries*sizeof(journalentry) >= journalMapLen) {
+				// die Journal-Datei muss vergrößert und erneut gemappt werden 
+					munmap(journalMapAdd, journalMapLen);
+					journalMapAdd = mapFile(fileno(journalFile),journalMapLen, auxSpace, &journalMapLen);
+					journalMapCurrentAdd = journalMapAdd + journalEntries*sizeof(journalentry);
+				}
+				//printf("%li;%32s;%i\n",record.block, md5String, record.len);
+			} else { // DER HASH IST BEREITS BEKANNT
+	//printf(".");
+				infoForMetaFile = hashInJournalPos;
+			}
+			// Informationen ins Metafile schreiben
+			fprintf(metaFile, "%ld\n", infoForMetaFile);
+			if(metaFileChanged) {
+				newBytes += current_read;
+				storageFileLen += current_read;
+			}
+			bytesRead += current_read;
+			if(run++%500==0) {
+				printf("+");
+				fflush(stdout);
+			}	
 		}
-		// Informationen ins Metafile schreiben
-		fprintf(metaFile, "%ld\n", infoForMetaFile);
-		if(metaFileChanged) {
-			newBytes += current_read;
-			storageFileLen += current_read;
-		}
-		bytesRead += current_read;
-		if(run++%500==0) {
-			printf("+");
-			fflush(stdout);
-		}	
+
+
+		if(inputFileBuffer) free(inputFileBuffer);
 	}
+	
+
+	// Nachbereitung
 	munmap(journalMapAdd, journalMapLen);
 	/* Datei wieder verkleinern */
 	if(ftruncate(fileno(journalFile),journalEntries*sizeof(journalentry))==-1) {
